460b02c27512712761e57cc88af75a9333977d3e
Khurshed2008 <k2304069@gmail.com>
2023-03-04 19:00:45 +0500

    hello

--- app/src/main/java/com/example/my_project_1/MainActivity.kt
+++ app/src/main/java/com/example/my_project_1/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.my_project_1
 
+import android.content.IntentSender.OnFinished
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        object: CountDownTimer(10000,1000){
+            override fun onTick(milisUntilFinished: Long) {
+                println(milisUntilFinished / 1000)
+                TODO("Not yet implemented")
+            }
+            override fun onFinish() {
+                TODO("Not yet implemented")
+                println("It is finished")
+                println("hello")
+            }
+        }
     }
 }
